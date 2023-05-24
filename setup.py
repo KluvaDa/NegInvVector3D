b4fb12bed4a9a3317e85f6e755623aaa34467a0c
KluvaDa <daniel.kluvanec@durham.ac.uk>
2023-05-24 16:37:17 +0100

    Update setup.py
    Changed package name to neginvvector3d

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
 setup(
-    name='niv',
+    name='neginvvector3d',
     version='1.0.0',
-    py_modules=['niv'],
+    py_modules=['neginvvector3d'],
     install_requires=['torch']
 )
